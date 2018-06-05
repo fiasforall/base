7d94baf35c0547cb4ae4c28775271f98879bf8dd
Barnes Harris <fiasforall@yandex.ru>
2018-06-05 15:53:07 +0300

    +PK(actstatid)

--- db/tables/ACTSTAT_ddl.sql
+++ db/tables/ACTSTAT_ddl.sql
@@ -2,3 +2,7 @@
     actstatid number not null,
     name      varchar2(100 char)
 ) tablespace fias_tbl;
+alter table ACTSTAT add constraint PK_ACTSTAT
+    primary key (actstatid) 
+    using index tablespace fias_idx;
+
